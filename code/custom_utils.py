b3f5cc0ec1a16769e93ab7eddfc5e2b81638542c
Zewei (Whiskey) Liao <wliao0504@uchicago.edu>
2024-06-28 17:29:46 -0500

    finished mapping first item

--- code/custom_utils.py
+++ code/custom_utils.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import gzip
 
-# def load_gz_csv(file_path):
-#     with gzip.open(file_path, 'rt', encoding = 'utf-8') as f:
-#         df = pd.read_csv(f)
-#     return df
+def load_gz_csv(file_path):
+    with gzip.open(file_path, 'rt', encoding = 'utf-8') as f:
+        df = pd.read_csv(f)
+    return df
 
 def lookup_item(df, kw: str, col: str = "label", case: bool = False):
     '''
@@ -14,5 +14,4 @@ def lookup_item(df, kw: str, col: str = "label", case: bool = False):
     output = df[
         df[col].str.contains(kw, case = case, na = False)
     ]
-    
     return output
