ae90bc116171eef20977265410eca80ecd8be8a0
Zewei (Whiskey) Liao <wliao0504@uchicago.edu>
2024-06-28 01:06:06 -0500

    added some helper functions in custom_utils.py

---
+++ code/custom_utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import gzip
+
+# def load_gz_csv(file_path):
+#     with gzip.open(file_path, 'rt', encoding = 'utf-8') as f:
+#         df = pd.read_csv(f)
+#     return df
+
+def lookup_item(df, kw: str, col: str = "label", case: bool = False):
+    '''
+    col = {"label", "abbr"}
+    '''
+    if col == "abbr": col == "abbreviation"
+    output = df[
+        df[col].str.contains(kw, case = case, na = False)
+    ]
+    
+    return output
