0004305e559bea597989b70dc16d94a3be0ff16f
Zewei (Whiskey) Liao <wliao0504@uchicago.edu>
2024-08-01 23:03:02 -0500

    switching to local dev or rss since ssh tunneling hasn't been working on randi

--- code/eda.py
+++ code/eda.py
@@ -1,9 +1,1 @@
-import pandas as pd
-import gzip
-
-def load_gz_csv(file_path):
-    with gzip.open(file_path, 'rt', encoding = 'utf-8') as f:
-        df = pd.read_csv(f)
-    return df
-
-chartevents = load_gz_csv("../mimic-iv-2.2/icu/chartevents.csv.gz")
+print("x")
