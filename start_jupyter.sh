c63b68d38eb9d3f6c9d9daf2f4cbb59c563cbde3
zliao <wliao0504@uchicago.edu>
2024-11-03 18:08:50 -0600

    finished labs table (only short of three labs that are not used for ATS)

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -5,7 +5,7 @@ PART_SUFFIX=${1:-2q}  # Default to "2q" if no argument is provided
 PARTITION="tier${PART_SUFFIX}"
 
 # Request resources and start an interactive session with the specified or default partition
-srun --pty -p "$PARTITION" --mem 150G --time 30:00:00 /bin/bash << EOF
+srun -p "$PARTITION" --mem 150G --time 30:00:00 /bin/bash << EOF
 
 # Load necessary modules
 module load gcc/12.1.0 python/3.10.5
